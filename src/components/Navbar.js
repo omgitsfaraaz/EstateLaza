4d8d9da25fa79449081b0c21664f723deaae2126
unknown <mohsinsait23@gmail.com>
2021-01-08 19:58:46 +0530

    added color and height using styled-components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+    height: 60px;
+    background: red;
+`;
 
 const Navbar = () => {
     return (
-        <h1>Navbar</h1>
+        <Nav>
+            <h1>Navbar</h1>
+        </Nav>
     )
 }
 
