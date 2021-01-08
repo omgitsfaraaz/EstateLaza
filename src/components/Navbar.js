f9a327b3322afe429203ae7861ec33c06f13fca9
unknown <mohsinsait23@gmail.com>
2021-01-08 20:27:29 +0530

    changed background color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { menuData } from '../data/MenuData';
 
 const Nav = styled.nav`
     height: 60px;
-    background: red;
+    background: #000;
 `;
 
 const Logo = styled(Link)`
     color: #fff;
 `;
 
-const MenuBars = styled.i``
-const NavMenu = styled.i``
+const MenuBars = styled.i``;
+
+const NavMenu = styled.div``;
+
+const NavMenuLinks = styled(Link)`
+    color: #fff;
+`;
 
 const Navbar = () => {
     return (
         <Nav>
-            <Logo>ESTATELAZA</Logo>
+            <Logo to="/">ESTATELAZA</Logo>
             <MenuBars />
             <NavMenu>
-
+                {menuData.map((item, index) => {
+                    <NavMenuLinks to={item.link} key={index}>
+                        {item.title}
+                    </NavMenuLinks>
+                })}
             </NavMenu>
             <h1>Navbar</h1>
         </Nav>
