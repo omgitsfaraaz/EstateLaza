0b7474db5f20679914b101de35c3f981e2a65cda
unknown <mohsinsait23@gmail.com>
2021-01-08 20:31:05 +0530

    added navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import { menuData } from '../data/MenuData';
 
 const Nav = styled.nav`
     height: 60px;
+    display: flex;
     background: #000;
 `;
 
@@ -26,11 +27,11 @@ const Navbar = () => {
             <Logo to="/">ESTATELAZA</Logo>
             <MenuBars />
             <NavMenu>
-                {menuData.map((item, index) => {
+                {menuData.map((item, index) => (
                     <NavMenuLinks to={item.link} key={index}>
                         {item.title}
                     </NavMenuLinks>
-                })}
+                ))}
             </NavMenu>
             <h1>Navbar</h1>
         </Nav>
