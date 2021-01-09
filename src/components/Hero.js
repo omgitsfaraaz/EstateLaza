d61e88750aa6f05b5531413b9db8ed59388e385b
unknown <mohsinsait23@gmail.com>
2021-01-09 13:10:43 +0530

    added next and previous buttons

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { FaSlideshare } from 'react-icons/fa';
 import styled, { css } from 'styled-components/macro';
 import { Button } from './Button';
+import { IoMdArrowRoundForward } from 'react-icons/io';
+import { IoArrowForward, IoArrowBack } from 'react-icons/io5';
 
 const HeroSection = styled.section`
     height: 100vh;
@@ -20,10 +21,45 @@ const HeroWrapper = styled.div`
     position: relative;
 `;
 
-const HeroSlide = styled.div``
-const HeroSlider = styled.div``
-const HeroImage = styled.div``
-const HeroContent = styled.div``
+const HeroSlide = styled.div``;
+const HeroSlider = styled.div``;
+const HeroImage = styled.img``;
+const HeroContent = styled.div``;
+const Arrow = styled(IoMdArrowRoundForward)``;
+
+const SliderButtons = styled.div`
+    position: absolute;
+    bottom: 50px;
+    right: 50px;
+    display: flex;
+    z-index: 10;
+`;
+
+const arrowButtons = css`
+    width: 50px;
+    height:50px;
+    color: #fff;
+    cursor: pointer;
+    background: #000d1a;
+    border-radius: 50px;
+    padding: 10px;
+    margin-right: 1rem;
+    user-select: none;
+    transition: 0.3s;
+
+    &:hover {
+        background: #cd853f;
+        transform: scale(1.05);
+    }
+`;
+
+const PrevArrow = styled(IoArrowBack)`
+    ${arrowButtons}
+`;
+
+const NextArrow = styled(IoArrowForward)`
+    ${arrowButtons}
+`;
 
 const Hero = ({ slides }) => {
     return (
@@ -45,12 +81,17 @@ const Hero = ({ slides }) => {
                                         `}
                                     >
                                         {slide.label}
+                                        <Arrow />
                                     </Button>
                                 </HeroContent>
                             </HeroSlider>
                         </HeroSlide>
                     )
                 })}
+                <SliderButtons>
+                    <PrevArrow />
+                    <NextArrow />
+                </SliderButtons>
             </HeroWrapper>
         </HeroSection>
     )
