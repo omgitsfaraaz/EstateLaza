c140bccdb833a619aa1d752a71fe103aacf26ced
unknown <mohsinsait23@gmail.com>
2021-01-14 16:24:50 +0530

    styled images to shape as polygon

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import imageOne from '../images/img-1.jpg';
+import imageTwo from '../images/img-2.jpg';
+import imageThree from '../images/img-3.jpg';
+import imageFour from '../images/img-4.jpg';
+import imageFive from '../images/img-5.jpg';
+import imageSix from '../images/img-6.jpg';
+import imageSeven from '../images/img-7.jpg';
+import '../App.css';
 
 const Services = () => {
     return (
@@ -14,27 +21,27 @@ const Services = () => {
                     <div className="services-cell_text">Development</div>
                 </div>
                 <div className="services-cell">
-                    <img className="services-cell_img" alt="" src="" />
+                    <img className="services-cell_img" alt="" src={imageTwo} />
                     <div className="services-cell_text">Sales</div>
                 </div>
                 <div className="services-cell">
-                    <img className="services-cell_img" alt="" src="" />
+                    <img className="services-cell_img" alt="" src={imageThree} />
                     <div className="services-cell_text">Brokerage</div>
                 </div>
                 <div className="services-cell">
-                    <img className="services-cell_img" alt="" src="" />
+                    <img className="services-cell_img" alt="" src={imageFour} />
                     <div className="services-cell_text">Property Management</div>
                 </div>
                 <div className="services-cell">
-                    <img className="services-cell_img" alt="" src="" />
+                    <img className="services-cell_img" alt="" src={imageFive} />
                     <div className="services-cell_text">Lending</div>
                 </div>
                 <div className="services-cell">
-                    <img className="services-cell_img" alt="" src="" />
+                    <img className="services-cell_img" alt="" src={imageSix} />
                     <div className="services-cell_text">Professional Services</div>
                 </div>
                 <div className="services-cell">
-                    <img className="services-cell_img" alt="" src="" />
+                    <img className="services-cell_img" alt="" src={imageSeven} />
                     <div className="services-cell_text">Marketing</div>
                 </div>
             </div>
